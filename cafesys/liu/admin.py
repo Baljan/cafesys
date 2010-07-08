bdee1ef29b484f5f28050798dbb4a57cf487f02a
Simon Pantzare <simon@spantz.org>
2010-07-08 22:46:33 +0200

    models to liu app, initial fixtures

---
+++ cafesys/liu/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from models import Role, Student
+
+admin.site.register(Role)
+admin.site.register(Student)
+
--- cafesys/liu/models.py
+++ cafesys/liu/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Role(models.Model):
+    """Board member, worker, regular, and so on."""
+    title = models.CharField(max_length=20, unique=True)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "%s: %s" % (self.title, self.description)
+
+class Student(models.Model):
+    liu_id = models.CharField(max_length=8, unique=True)
+    balance = models.IntegerField(default=0)
+    role = models.ForeignKey(Role)
+
+    def __str__(self):
+        return "%s (%s)" % (self.liu_id, self.role.title)
