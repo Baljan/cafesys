a414f1f567cfe2115823d4cd52f20f2500ccb503
Simon Pantzare <simon@spantz.org>
2010-07-10 20:02:55 +0200

    more work on presentation of the calendar

--- cafesys/cal/views.py
+++ cafesys/cal/views.py
@@ -10,15 +10,20 @@ from django.views.decorators.csrf import csrf_exempt
 from datetime import datetime
 import calendar
 
+from models import ScheduledMorning, ScheduledAfternoon, MorningShift, AfternoonShift
+
 def worker_calendar(request, year=None, month=None):
     now = datetime.now()
     if year is None:
         year = now.year
+    else:
+        year = int(year)
 
     if month is None:
         year_view = True
     else:
         year_view = False
+        month = int(month)
 
     months = []
     if year_view:
@@ -26,8 +31,27 @@ def worker_calendar(request, year=None, month=None):
             months.append((year, m, calendar.monthcalendar(year, m)))
     else:
         months.append((year, month, calendar.monthcalendar(year, month)))
+    
+    for mid, (y, m, week_data) in enumerate(months):
+        ms = ScheduledMorning.objects.filter(
+                shift__day__year=y, 
+                shift__day__month=m,
+                )
+        afs = ScheduledAfternoon.objects.filter(
+                shift__day__year=y, 
+                shift__day__month=m,
+                )
+        for wid, w in enumerate(week_data):
+            for did, day in enumerate(w):
+                to = {'day': day, 'morning': [], 'afternoon': []}
+                if day != 0:
+                    to.update({
+                        'morning': list([x.student.liu_id for x in ms if x.shift.day.day==day]), 
+                        'afternoon': list([x.student.liu_id for x in afs if x.shift.day.day==day]), 
+                        })
+                months[mid][2][wid][did] = to
 
     return render_to_response('calendar/calendar.html', {
-        'months': months,
+        'calendar': months,
         'year_view': year_view,
         }, context_instance=RequestContext(request))
