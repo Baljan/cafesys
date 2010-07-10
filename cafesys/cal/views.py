50885e8e363c2d5dde3af53e40db7585d19372d3
Simon Pantzare <simon@spantz.org>
2010-07-10 21:37:40 +0200

    show week numbers

--- cafesys/cal/views.py
+++ cafesys/cal/views.py
@@ -21,15 +21,18 @@ def worker_calendar(request, year=None, month=None):
 
     if month is None:
         year_view = True
+        month = now.month
     else:
         year_view = False
         month = int(month)
 
     months = []
     if year_view:
+        first_week = int(datetime(year, 1, 1).strftime('%W'))
         for m in range(1, 12+1):
             months.append((year, m, calendar.monthcalendar(year, m)))
     else:
+        first_week = int(datetime(year, month, 1).strftime('%W'))
         months.append((year, month, calendar.monthcalendar(year, month)))
     
     for mid, (y, m, week_data) in enumerate(months):
@@ -42,14 +45,19 @@ def worker_calendar(request, year=None, month=None):
                 shift__day__month=m,
                 )
         for wid, w in enumerate(week_data):
+            week_info = {
+                    'number': first_week + wid,
+                    'days': months[mid][2][wid],
+                    }
             for did, day in enumerate(w):
-                to = {'day': day, 'morning': [], 'afternoon': []}
+                to = {'dayno': did, 'day': day, 'morning': [], 'afternoon': []}
                 if day != 0:
                     to.update({
                         'morning': list([x.student.liu_id for x in ms if x.shift.day.day==day]), 
                         'afternoon': list([x.student.liu_id for x in afs if x.shift.day.day==day]), 
                         })
-                months[mid][2][wid][did] = to
+                week_info['days'][did] = to
+            months[mid][2][wid] = week_info
 
     return render_to_response('calendar/calendar.html', {
         'calendar': months,
