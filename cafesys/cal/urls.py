89e5daa3bb591a216689e48aebcb5c2f13002e52
Simon Pantzare <simon@spantz.org>
2010-07-17 22:29:06 +0200

    url to swappable view

--- cafesys/cal/urls.py
+++ cafesys/cal/urls.py
@@ -6,5 +6,6 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns("",
     url(r"^$", 'cal.views.worker_calendar'),
+    url(r"^swappable$", 'cal.views.swappable'),
     url(r"^(?P<year>\d+)/(?P<month>.*)$", 'cal.views.worker_calendar'),
 )
