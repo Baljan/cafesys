1bc38687b4eee9db159a9885d62d3f19d0a73ffe
Simon Pantzare <simon@spantz.org>
2010-07-24 15:15:34 +0200

    test logins for all kinds of users

--- cafesys/liu/tests.py
+++ cafesys/liu/tests.py
@@ -7,6 +7,7 @@ Replace these with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.test.client import Client
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,6 +15,25 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.failUnlessEqual(1 + 1, 2)
+
+    def _login_test_with_params(self, username_and_password, is_regular=False,
+            is_worker=False, is_board_member=False):
+        c = Client()
+        c.login(username=username_and_password, password=username_and_password)
+        r = c.get('/calendar/')
+        self.failUnlessEqual(r.context['is_regular'], is_regular)
+        self.failUnlessEqual(r.context['is_worker'], is_worker)
+        self.failUnlessEqual(r.context['is_board_member'], is_board_member)
+
+    def test_login_as_regular(self):
+        self._login_test_with_params('regular', is_regular=True)
+
+    def test_login_as_worker(self):
+        self._login_test_with_params('worker', is_worker=True)
+
+    def test_login_as_board(self):
+        self._login_test_with_params('board', is_worker=True, is_board_member=True)
+
 
 __test__ = {"doctest": """
 Another way to test that 1 + 1 is equal to 2.
