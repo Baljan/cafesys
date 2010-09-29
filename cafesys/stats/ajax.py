991d71465be29772e94c2f71d7efe64296dbf158
Simon Pantzare <simon@spantz.org>
2010-09-29 21:12:58 +0200

    work on a "Orders per Day and Hour" view (working but needs testing)

--- cafesys/stats/ajax.py
+++ cafesys/stats/ajax.py
@@ -9,6 +9,9 @@ from datetime import datetime, MINYEAR, MAXYEAR
 from django.utils.translation import ugettext as _ 
 from liu import is_worker, is_board_member
 import numpy
+from django.core import serializers
+import json
+import itertools
 
 log = logging.getLogger('stats')
 
@@ -27,19 +30,27 @@ def day_hour_order_func(
         return len(matches)
     return func
 
+
 def orders_per_day_and_hour(request, start_date=None, end_date=None):
-    """Returns a 7-element array where each element is a 24-element array where
+    """Returns a 5-element array where each element is a 10-element array where
     each value is the number of orders taken on that hour and weekday. The
     values are the totals for the range specified.
     """
     assert is_board_member(request)
     f = day_hour_order_func()
-    days_and_hours = numpy.zeros((7, 24), int)
+    days_and_hours = numpy.zeros((5, 10), int)
+    maxval = 0
     for di, day in enumerate(days_and_hours):
         for hi, hour in enumerate(day):
-            days_and_hours[di][hi] = f(di, hi)
+            val = f(di, hi+8)
+            maxval = max(maxval, val)
+            days_and_hours[di][hi] = val
+    flat = list(itertools.chain.from_iterable(days_and_hours.tolist()))
+    return json.dumps({
+        'flat': flat,
+        'maxval': maxval,
+        })
 
-    from pprint import pprint
-    pprint(days_and_hours)
+
 
 dajaxice_functions.register(orders_per_day_and_hour)
