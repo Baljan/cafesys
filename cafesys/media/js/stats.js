ff1908660c84f9e77d7b1c7f88b497a6dbbe443c
Simon Pantzare <simon@spantz.org>
2010-09-29 20:17:21 +0200

    dummy call

--- cafesys/media/js/stats.js
+++ cafesys/media/js/stats.js
@@ -1,2 +1,10 @@
+function processOrdersPerDayAndHour(data)
+{
+    Dajax.process(data);
+    alert(data);
+}
+
 $(document).ready(function () {
+    Dajaxice.stats.orders_per_day_and_hour('processOrdersPerDayAndHour', {
+    });
 });
