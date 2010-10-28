c02073c0975c2c01aa5773cc59e120bd4a90d920
Simon Pantzare <simon@spantz.org>
2010-10-28 02:57:04 +0200

    add settings

--- cafesys/settings.py
+++ cafesys/settings.py
@@ -50,10 +50,18 @@ DATABASES = {
 # system time zone.
 TIME_ZONE = "Europe/Stockholm"
 
+USE_L10N = True
+
+
+#FORMAT_MODULE_PATH = 'formats' # FIXME: not working
+
 DATE_FORMAT = 'Y-m-d'
 DATETIME_FORMAT = 'Y-m-d H:i'
 TIME_FORMAT = 'H:i'
-USE_L10N = True
+
+# FIXME: These two settings have no effect.
+NUMBER_GROUPING = 3
+THOUSAND_SEPARATOR = ' '
 
 # Language code for this installation. All choices can be found here:
 # http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
