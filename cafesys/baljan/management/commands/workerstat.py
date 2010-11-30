fd4cd5915c12aa73af126a6e2af95a59db59bd93
Simon Pantzare <simon@spantz.org>
2010-11-30 16:08:39 +0100

    formatting

--- cafesys/baljan/management/commands/workerstat.py
+++ cafesys/baljan/management/commands/workerstat.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 from baljan.util import get_logger
@@ -25,7 +26,11 @@ class Command(BaseCommand):
         workers.sort(key=lambda x: x.shiftsignup_set.all().count(), reverse=True)
         signup_counts = sorted(set([w.shiftsignup_set.all().count() for w in workers]), reverse=True)
         print "%d worker(s):" % len(workers)
+        tot_count = 0
         for signup_count in signup_counts:
             workers_with_count_signups = [w for w in workers 
                     if w.shiftsignup_set.all().count()==signup_count]
-            print "%3d signup(s): %3d" % (signup_count, len(workers_with_count_signups))
+            c = len(workers_with_count_signups)
+            tot_count += c
+            print "%3d shift(s): %3d (%.2G%%)" % (signup_count, c, 100.0*c/len(workers))
+        assert tot_count == len(workers)
