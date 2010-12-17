50ef7b9f8796c0485a39a560366411008032b696
Simon Pantzare <simon@spantz.org>
2010-12-17 16:08:00 +0100

    more logging

--- cafesys/baljan/card2user/manualdb.py
+++ cafesys/baljan/card2user/manualdb.py
@@ -2,9 +2,16 @@
 from django.conf import settings
 from django.contrib.auth.models import User, Group
 
+from baljan.util import get_logger
+
+log = get_logger('baljan.card2user.manualdb', with_sentry=False)
+
 class Finder(object):
     def search(self, card_id):
         try:
-            return User.objects.get(profile__card_id=card_id)
+            user = User.objects.get(profile__card_id=card_id)
+            log.info('found %s in local db' % user)
+            return user
         except:
+            log.info('owner of card %s could not be fetched' % card_id)
             return None
