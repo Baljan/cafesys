a3d3644094ac4ca2c8120bd32086d5dc5a9a5184
Simon Pantzare <simon@spantz.org>
2011-01-18 02:08:45 +0100

    dumb caching

--- cafesys/baljan/stats.py
+++ cafesys/baljan/stats.py
@@ -3,6 +3,7 @@ from baljan.models import Order
 from django.db.models import Avg, Count, Max, Min
 from django.contrib.auth.models import User, Permission, Group
 from datetime import datetime
+from django.core.cache import cache
 
 def top_consumers(start=None, end=None, simple=False):
     """`start` and `end` are dates. Returns top consumers in the interval with
@@ -13,13 +14,20 @@ def top_consumers(start=None, end=None, simple=False):
     if end is None:
         end = datetime(2999, 1, 1, 0, 0)
 
-    top = User.objects.filter(
-        profile__show_profile=True,
-        order__put_at__gte=start,
-        order__put_at__lte=end,
-    ).annotate(
-        num_orders=Count('order'),
-    ).order_by('-num_orders')
+    fmt = '%Y-%m-%d'
+    key = 'baljan.stats.start-%s.end-%s' % (start.strftime(fmt), end.strftime(fmt))
+    top = cache.get(key)
+    if top is None:
+        top = User.objects.filter(
+            profile__show_profile=True,
+            order__put_at__gte=start,
+            order__put_at__lte=end,
+        ).annotate(
+            num_orders=Count('order'),
+        ).order_by('-num_orders')
+
+        quarter = 60 * 15 # seconds
+        cache.set(key, top, quarter)
 
     if simple:
         simple_top = []
