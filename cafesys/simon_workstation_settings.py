5e39d7b6254769859c2390fc45434e144f925160
Simon Pantzare <simon@pewpewlabs.com>
2011-07-29 20:01:10 +0200

    update local settings

--- cafesys/simon_workstation_settings.py
+++ cafesys/simon_workstation_settings.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-DEBUG = False
+DEBUG = True
+LDAP_ENABLED = False
 STATS_CACHE = True
 
 DATABASES = {
@@ -8,8 +9,8 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
         "NAME": "cafesys",                       # Or path to database file if using sqlite3.
         "TEST_NAME": "test_cafesys",
-        "USER": "django",                             # Not used with sqlite3.
-        "PASSWORD": "django",                         # Not used with sqlite3.
+        "USER": "sp",                             # Not used with sqlite3.
+        "PASSWORD": "",                         # Not used with sqlite3.
         "HOST": "",                             # Set to empty string for localhost. Not used with sqlite3.
         "PORT": "",                             # Set to empty string for default. Not used with sqlite3.
     }
@@ -22,7 +23,7 @@ EMAIL_PORT = 25
 EMAIL_USE_TLS = False
 DEFAULT_FROM_EMAIL = 'noreply@ejlert.spantz.org'
 
-SITE_ID=2
+SITE_ID=1
 
 # For importing data from the old system. There is a management command
 # that uses these settings (importoldsystem).
