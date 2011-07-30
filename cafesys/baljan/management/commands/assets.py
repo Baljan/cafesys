33a2aa418b08d7c6bf7ff0341bc53e8b7a620996
Simon Pantzare <simon@pewpewlabs.com>
2011-07-30 15:29:04 +0200

    helper command for assets (calls jammit internally)

---
+++ cafesys/baljan/management/commands/assets.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from subprocess import call
+
+from django.core.management.base import BaseCommand, CommandError
+from django.core.management import call_command
+
+class Command(BaseCommand):
+    help = "Collect static files and create assets."
+    
+    def handle(self, **options):
+        call_command('collectstatic', link=True, interactive=False)
+        call(['jammit', '--output', 'jammit/assets', '--force'])
