6060bcc48a9fa52063e431c0aa656ce43ad04dc6
Simon Pantzare <simon@pewpewlabs.com>
2011-09-09 20:15:52 +0200

    bump static version

--- cafesys/settings.py
+++ cafesys/settings.py
@@ -67,7 +67,7 @@ LANGUAGES = (
 
 # Bump when for example CSS or JS files change to force clients to download a
 # new version.
-MEDIA_AND_STATIC_VERSION = 13
+MEDIA_AND_STATIC_VERSION = 14
 
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media")
 MEDIA_URL = "/media%d/" % MEDIA_AND_STATIC_VERSION
