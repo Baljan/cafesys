a8bf950cd12689ed2c392a761e03529155220742
Simon Pantzare <simon@spantz.org>
2012-09-02 12:59:00 +0200

    Add bookkeep mgmt. command

---
+++ cafesys/baljan/management/commands/bookkeep.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import os
+import datetime
+
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
+from baljan.models import User
+from baljan.models import BalanceCode
+from baljan.models import Order
+
+
+class Command(BaseCommand):
+    args = 'year'
+    help = 'Show customer debt for year.'
+
+    def handle(self, *args, **options):
+        if len(args) != 1:
+            raise CommandError('need a year')
+
+        year = int(args[0])
+        year_start = datetime.datetime(year, 1, 1)
+        year_end = datetime.datetime(year, 12, 31)
+
+        users = User.objects.filter(order__put_at__gte=year_start).distinct()
+        excluded_balance_codes_sum = 0
+        excluded_orders_sum = 0
+        for user in users:
+            user_excluded_balance_codes = user.balancecode_set.filter(used_at__gt=year_end).distinct()
+            for balance_code in user_excluded_balance_codes:
+                excluded_balance_codes_sum += balance_code.value
+            user_excluded_orders = user.order_set.filter(put_at__gt=year_end).distinct()
+            for order in user_excluded_orders:
+                excluded_orders_sum += order.paid
+        current_users_total_balance = sum([user.profile.balance for user in users])
+        accumulated_excluded = excluded_orders_sum - excluded_balance_codes_sum
+
+        print "users", len(users)
+        print "excluded balance codes sum", excluded_balance_codes_sum
+        print "excluded orders sum", excluded_orders_sum
+        print "current total balance", current_users_total_balance
+        print "accumulated excluded", accumulated_excluded
+        print "total balance in year", current_users_total_balance + accumulated_excluded
