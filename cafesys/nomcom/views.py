e39ce61ee65736ae470ae4ba9af13627c181cf6d
Olle Vidner <olle@vidner.se>
2016-09-19 17:27:39 +0200

    Removed dead code

--- cafesys/nomcom/views.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-def apply_board(request):
-    return render_to_response('nomcom/apply_board.html', {}, context_instance=RequestContext(request))
--- parport/parapin-1.5.1-beta1/parapin_rb/extconf.rb
+++
@@ -1,7 +0,0 @@
-require 'mkmf'
-
-$CFLAGS = "#{$CFLAGS} -I.."
-
-dir_config("parapin")
-
-create_makefile("parapin")
