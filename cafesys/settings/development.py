7d8a05d5ed5291b54c3e795c660c1141c34c1e88
Olle Vidner <olle@vidner.se>
2017-05-25 23:45:24 +0200

    Simplified settings

--- cafesys/settings/development.py
+++ cafesys/settings/development.py
@@ -1,18 +1,6 @@
 # -*- coding: utf-8 -*-
-from . import *
-
-DEBUG = env.bool('DJANGO_DEBUG', default=True)
-TEMPLATE_DEBUG = DEBUG
-
-SECRET_KEY = env.str('DJANGO_SECRET_KEY', default='55qj2y&$zh_1rsxs5(ibkg8y)t=ewo(ln5d)%l(u_^xp$*=^f+')
-
-_EMAIL_CONFIG = env.email_url('DJANGO_EMAIL_URL', default='consolemail://')
-EMAIL_BACKEND = _EMAIL_CONFIG.get('EMAIL_BACKEND')
-EMAIL_HOST = _EMAIL_CONFIG.get('EMAIL_HOST')
-EMAIL_HOST_USER = _EMAIL_CONFIG.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = _EMAIL_CONFIG.get('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = _EMAIL_CONFIG.get('EMAIL_PORT')
-EMAIL_USE_TLS = _EMAIL_CONFIG.get('EMAIL_USE_TLS', True)
-DEFAULT_FROM_EMAIL = CONTACT_EMAIL
+from . import *  # noqa
 
 SERVE_MEDIA = True
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
