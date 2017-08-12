43beddaf564ba2f0c0e5f38e3d7f8d08e5d1e482
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-08-12 20:24:57 +0200

    Redirect to www.baljan.org automatically

--- cafesys/settings/production.py
+++ cafesys/settings/production.py
@@ -4,3 +4,7 @@ from . import *  # noqa
 SERVE_MEDIA = False
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
+
+# The new login flow requires that the login request originates from
+# www.baljan.org, so we must ensure that our users go there automatically.
+PREPEND_WWW = True
