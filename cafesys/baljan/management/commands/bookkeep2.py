6f00a00565cf341220c7dec897836ececa538e3c
rosakosa <rosa_kosa93@hotmail.com>
2017-11-16 22:45:05 +0100

    Updated command bookkeep2.

--- cafesys/baljan/management/commands/bookkeep2.py
+++ cafesys/baljan/management/commands/bookkeep2.py
@@ -19,21 +19,32 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         year = options['year']
         year_start = datetime.datetime(year, 1, 1)
-        year_end = datetime.datetime(year, 12, 31)
+        year_end = datetime.datetime(year, 12, 31, 23, 59,59)
 
         balance_code_sum = BalanceCode.objects.filter(used_at__range=(year_start, year_end)).aggregate(Sum('value'))
         active_balance = Profile.objects.filter(user__order__put_at__range=(year_start, year_end)).distinct().aggregate(Sum('balance'))
-        unused_balance = Profile.objects.exclude(user__order__put_at__range=(year_start, year_end)).distinct().aggregate(Sum('balance'))
+        unused_balance_before = Profile.objects.filter(user__order__put_at__lt=year_start).exclude(user__order__put_at__range=(year_start, year_end)).distinct().aggregate(Sum('balance'))
+        unused_balance_after = Profile.objects.filter(user__order__put_at__gt=year_end).exclude(user__order__put_at__range=(year_start, year_end)).distinct().aggregate(Sum('balance'))
+        unused_balance_dubble = Profile.objects.filter(user__order__put_at__lt=year_start).filter(user__order__put_at__gt=year_end).exclude(user__order__put_at__range=(year_start, year_end)).distinct().aggregate(Sum('balance'))
+        total_balance = Profile.objects.all().aggregate(Sum('balance'))
 
         if balance_code_sum["value__sum"] is None:
             balance_code_sum["value__sum"] = 0
         if active_balance["balance__sum"] is None:
             active_balance["balance__sum"] = 0
-        if unused_balance["balance__sum"] is None:
-            unused_balance["balance__sum"] = 0
+        if unused_balance_before["balance__sum"] is None:
+            unused_balance_before["balance__sum"] = 0
+        if unused_balance_after["balance__sum"] is None:
+            unused_balance_after["balance__sum"] = 0
+        if unused_balance_dubble["balance__sum"] is None:
+            unused_balance_dubble["balance__sum"] = 0
+        if total_balance["balance__sum"] is None:
+            total_balance["balance__sum"] = 0
 
         print("Bokföringsinformation för år", year)
-        print("Summan för aktiverade kaffekorten under året:", balance_code_sum["value__sum"], "SEK")
-        print("Kontosumman på de som blippade under året:", active_balance["balance__sum"], "SEK")
-        print("Kontosuman för de som inte blippade under året:", unused_balance["balance__sum"], "SEK")
-        print("Kontosumman för alla konton:", active_balance["balance__sum"]+unused_balance["balance__sum"], "SEK")
+        print("(1) Summan för aktiverade kaffekort under året:", balance_code_sum["value__sum"], "SEK")
+        print("(2) Kontosumman för de som blippat under året:", active_balance["balance__sum"], "SEK")
+        print("(3) Kontosumman för de som blippat tidigare men inte under detta år:", unused_balance_before["balance__sum"], "SEK")
+        print("(4) Kontosumman för de som blippat efter men inte under detta år:", unused_balance_after["balance__sum"], "SEK")
+        print("(5) Kontosumman för de som finns med i  både (3) och (4), dvs. haft blippuppehåll:", unused_balance_dubble["balance__sum"], "SEK")
+        print("(6) Kontosumman för alla konton (2)+(3)+(4)-(5)=(6):", total_balance["balance__sum"], "SEK")
