5fe9f73501b933a31a02579bea75ee1bd778ca87
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-11-29 15:20:32 +0100

    Increase connect timeout time per request

--- cafesys/baljan/phone.py
+++ cafesys/baljan/phone.py
@@ -116,7 +116,7 @@ def _build_46elks_response(phone_numbers):
 
         busy = _build_46elks_response(phone_numbers[1:])
         if busy:
-            data['timeout'] = '12'
+            data['timeout'] = '20'
             data['busy'] = busy
             data['failed'] = busy
 
