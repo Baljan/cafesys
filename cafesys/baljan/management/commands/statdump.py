f7f2c2f3b956b2bfd385f543c714380d18a914e8
Patrik Sletmo <patrik.sletmo@gmail.com>
2018-02-15 17:33:13 +0100

    Additional command fixes

--- cafesys/baljan/management/commands/statdump.py
+++ cafesys/baljan/management/commands/statdump.py
@@ -8,6 +8,29 @@ from django.core.management.base import BaseCommand, CommandError
 
 from ...models import Order
 
+import gc
+
+
+def queryset_iterator(queryset, chunksize=1000):
+    """
+    Iterate over a Django Queryset ordered by the primary key
+
+    This method loads a maximum of chunksize (default: 1000) rows in it's
+    memory at the same time while django normally would load all rows in it's
+    memory. Using the iterator() method only causes it to not preload all the
+    classes.
+
+    Note that the implementation of the iterator does not support ordered query sets.
+    """
+    pk = 0
+    last_pk = queryset.order_by('-pk')[0].pk
+    queryset = queryset.order_by('pk')
+    while pk < last_pk:
+        for row in queryset.filter(pk__gt=pk)[:chunksize]:
+            pk = row.pk
+            yield row
+        gc.collect()
+
 
 class Command(BaseCommand):
     """
@@ -28,15 +51,16 @@ class Command(BaseCommand):
         date_from = options['date_from']
         date_to = options['date_to'] + timedelta(hours=23, minutes=59, seconds=59)
 
-        orders = Order.objects.filter(put_at__range=(date_from, date_to)).order_by('put_at')
+        orders = Order.objects.filter(put_at__range=(date_from, date_to))
+        orders_queryset = queryset_iterator(orders)
         print('time, paid')
 
         iterations_since_sleep = 0
-        for order in orders:
+        for order in orders_queryset:
             print('%s, %d' % (order.put_at.strftime('%Y-%m-%d %H:%M:%S'), order.paid))
             iterations_since_sleep += 1
 
-            if iterations_since_sleep >= 1:
+            if iterations_since_sleep >= 1000:
                 iterations_since_sleep = 0
                 sys.stdout.flush()
                 sleep(0.05)
