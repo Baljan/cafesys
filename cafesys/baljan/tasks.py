ae0aa807013f5eaf7501390623dad2b1bc54786b
teori199 <teori199@student.liu.se>
2018-09-03 10:53:39 +0200

    Clean up code that has become redundant with new duty phone implementation.

--- cafesys/baljan/tasks.py
+++ cafesys/baljan/tasks.py
@@ -5,7 +5,6 @@ from django.conf import settings
 from django.core.cache import cache
 from logging import getLogger
 
-from cafesys.baljan import slack
 from . import stats
 
 logger = getLogger(__name__)
@@ -16,21 +15,3 @@ def update_stats():
     s = stats.Stats()
     data = [s.get_interval(i) for i in stats.ALL_INTERVALS]
     cache.set(settings.STATS_CACHE_KEY, data, settings.STATS_CACHE_TTL)
-
-
-@shared_task
-def send_missed_call_message(call_from, call_to):
-    slack_data = slack.compile_slack_message(
-        call_from,
-        call_to,
-        'failed'
-    )
-
-    slack_response = requests.post(
-        settings.SLACK_PHONE_WEBHOOK_URL,
-        json=slack_data,
-        headers={'Content-Type': 'application/json'}
-    )
-
-    if slack_response.status_code != 200:
-        logger.warning('Unable to post to Slack')
