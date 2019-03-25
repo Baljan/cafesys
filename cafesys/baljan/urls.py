9437ba7953f149232f4e557f15f54c176ac42b6b
Teodor Riddarhaage <teori199@student.liu.se>
2019-03-25 20:41:15 +0100

    Add location to work planning.

--- cafesys/baljan/urls.py
+++ cafesys/baljan/urls.py
@@ -13,6 +13,7 @@ urlpatterns = (
     url(r"^semester/$", views.current_semester, name='current_semester'),
     url(r"^day/(?P<day>[0-9-]+)$", views.day_shifts, name='day_shifts'),
     url(r"^semester/(?P<name>\w+)$", views.semester, name='semester'),
+    url(r"^semester/(?P<name>\w+)/(?P<loc>[0-9]+)$", views.semester, name='located_semester'),
     url(r"^admin-semester$", views.admin_semester, name='admin_semester'),
     url(r"^admin-semester/(\w+)$", views.admin_semester, name='admin_semester'),
 
