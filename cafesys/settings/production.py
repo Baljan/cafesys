cca3f94268ec3261d15d6e99ec5182176b17fc4e
Patrik Sletmo <patrik.sletmo@gmail.com>
2019-05-27 18:10:42 +0200

    Moved www redirect to traefik configuration

--- cafesys/settings/production.py
+++ cafesys/settings/production.py
@@ -5,9 +5,5 @@ SERVE_MEDIA = False
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
-# The new login flow requires that the login request originates from
-# www.baljan.org, so we must ensure that our users go there automatically.
-PREPEND_WWW = True
-
 # Send correct redirect_uri to login provider
 SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
