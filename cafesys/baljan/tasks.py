7fa075167e545bca98893cbdc83ece0300c213cd
Emil <emil19@users.noreply.github.com>
2022-05-09 22:04:26 +0200

    delay notifications, remove dead notification code

--- cafesys/baljan/tasks.py
+++ cafesys/baljan/tasks.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 import requests
 from celery import shared_task
+from ..celery import app
 from django.conf import settings
 from django.core.cache import cache
+from django.core.mail import send_mail
 from logging import getLogger
-
-from . import stats
 
 logger = getLogger(__name__)
 
 
+@app.task
+def send_mail_task(title, body, from_email, to_emails):
+    send_mail(title, body, from_email, to_emails)
+
 @shared_task
 def update_stats():
+    from . import stats
     for location in stats.ALL_LOCATIONS:
-        update_stats_for_location(location)
-
-
-def update_stats_for_location(location):
-    data = stats.compute_stats_for_location(location)
-    cache.set(stats.get_cache_key(location), data, settings.STATS_CACHE_TTL)
+        data = stats.compute_stats_for_location(location)
+        cache.set(stats.get_cache_key(location), data, settings.STATS_CACHE_TTL)
