155323194a7eddd356fb5f8f42366de3e940c9ec
Emil <emil19@users.noreply.github.com>
2022-10-06 22:08:58 +0200

    improve calendar with link to day

--- cafesys/baljan/ical.py
+++ cafesys/baljan/ical.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 import pytz
+from django.contrib.sites.models import Site
+from django.urls import reverse
 from django.conf import settings
 from django.utils.translation import ugettext as _
 from icalendar import Calendar, Event
 
+from .util import to_iso8601
 from .models import ShiftSignup, OnCallDuty, Located
 
 
@@ -37,15 +40,29 @@ def get_cafe_name_for(located):
     else:
         return 'Byttan'
 
+def make_event(signup_or_duty, times, type_name):
+    ev = Event()
+    start, end = times
+    current_site = Site.objects.get_current()
+    detail_path = reverse('day_shifts', kwargs={'day': to_iso8601(signup_or_duty.shift.when)})
+    detail_url = f'https://{current_site}{detail_path}'
+    ev.add('summary', f'{type_name} i {get_cafe_name_for(signup_or_duty.shift)}')
+    ev.add('dtstart', encode_dt(start), encode=False)
+    ev.add('dtend', encode_dt(end), encode=False)
+    ev.add('dtstamp', encode_dt(signup_or_duty.made), encode=False)
+    ev.add('location', item_location(signup_or_duty))
+    ev.add('description', f"Läs mer: {detail_url}")
+    return ev
+
 def for_user(user):
     """Returns an `icalendar.Calendar` object."""
     signups = ShiftSignup.objects.filter(
         user=user,
-    ).order_by('shift__when', 'shift__span').distinct()
+    ).select_related("shift").order_by('shift__when', 'shift__span').distinct()
 
     oncalls = OnCallDuty.objects.filter(
         user=user,
-    ).order_by('shift__when', 'shift__span').distinct()
+    ).select_related("shift").order_by('shift__when', 'shift__span').distinct()
 
     cal = Calendar()
     cal.add('prodid', '-//Baljan//Baljan Schedule//EN')
@@ -53,23 +70,11 @@ def for_user(user):
     cal.add('method', 'PUBLISH')
 
     for signup in signups:
-        ev = Event()
-        start, end = signup.shift.worker_times()
-        ev.add('summary', 'Jobba i %s' % (get_cafe_name_for(signup.shift), ))
-        ev.add('dtstart', encode_dt(start), encode=False)
-        ev.add('dtend', encode_dt(end), encode=False)
-        ev.add('dtstamp', encode_dt(signup.made), encode=False)
-        ev.add('location', item_location(signup))
+        ev = make_event(signup, signup.shift.worker_times(), "Jobba")
         cal.add_component(ev)
 
     for oncall in oncalls:
-        ev = Event()
-        start, end = oncall.shift.oncall_times()
-        ev.add('summary', 'Jour i %s' % (get_cafe_name_for(oncall.shift), ))
-        ev.add('dtstart', encode_dt(start), encode=False)
-        ev.add('dtend', encode_dt(end), encode=False)
-        ev.add('dtstamp', encode_dt(oncall.made), encode=False)
-        ev.add('location', item_location(oncall))
+        ev = make_event(oncall, oncall.shift.oncall_times(), "Jour")
         cal.add_component(ev)
 
     return cal.to_ical().decode('utf-8')
