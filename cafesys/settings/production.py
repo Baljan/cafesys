9b9fc3cf2496c83185bac73266d67defac3abb5b
Thomas Granbohm <thomas.granbohm@gmail.com>
2025-12-19 09:22:44 +0100

    Remove duplicate setting definitions

--- cafesys/settings/production.py
+++ cafesys/settings/production.py
@@ -24,25 +24,6 @@ CSRF_TRUSTED_ORIGINS = [
     "https://wrapped.baljan.org",
 ]
 
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-
-SESSION_COOKIE_SAMESITE = "None"
-CSRF_COOKIE_SAMESITE = "None"
-
-CORS_ALLOWED_ORIGINS = [
-    "https://baljan.org",
-    "https://www.baljan.org",
-    "https://blipp.baljan.org",
-    "https://wrapped.baljan.org",
-]
-CSRF_TRUSTED_ORIGINS = [
-    "https://baljan.org",
-    "https://www.baljan.org",
-    "https://blipp.baljan.org",
-    "https://wrapped.baljan.org",
-]
-
 # Send correct redirect_uri to login provider
 SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
 SOCIAL_AUTH_SANITIZE_REDIRECTS = True
